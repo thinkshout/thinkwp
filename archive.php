47a4778407349ea57156039a08646345963c3d8e
jamacon36 <4700339+jamacon36@users.noreply.github.com>
2021-08-16 13:29:26 -0400

    adding base archive php file

--- archive.php
+++ archive.php
@@ -13,6 +13,23 @@ $templates               = array( 'archive/archive-' . $post->post_type . '.twig
 $context                 = Timber::get_context();
 $context['is_archive']   = true;
 $context['archive_slug'] = str_replace( ' ', '-', strtolower( $context['wp_title'] ) );
+$context['title']        = 'Archive';
 $context['pagination']   = Timber::get_pagination();
+
+if ( is_day() ) {
+	$context['title'] = 'Archive: ' . get_the_date( 'D M Y' );
+} elseif ( is_month() ) {
+	$context['title'] = 'Archive: ' . get_the_date( 'M Y' );
+} elseif ( is_year() ) {
+	$context['title'] = 'Archive: ' . get_the_date( 'Y' );
+} elseif ( is_tag() ) {
+	$context['title'] = single_tag_title( '', false );
+} elseif ( is_category() ) {
+	$context['title'] = single_cat_title( '', false );
+	array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
+} elseif ( is_post_type_archive() ) {
+	$context['title'] = post_type_archive_title( '', false );
+	array_unshift( $templates, 'archive-' . get_post_type() . '.twig' );
+}
 
 Timber::render( $templates, $context );
